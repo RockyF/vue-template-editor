28e47fe05aab745a548597541da9100b59eb21e8
GitHub Action <action@github.com>
2022-10-08 07:23:46 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "vue-template-editor"
--- src/main/kotlin/com/github/rockyf/vuetemplateeditor/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rockyf/vuetemplateeditor/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rockyf.vuetemplateeditor.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rockyf.vuetemplateeditor.MyBundle
 
 class MyProjectService(project: Project) {
 
